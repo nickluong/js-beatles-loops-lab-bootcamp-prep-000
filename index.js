231d3e2427c4b687bd0e8a8fa1e0729bd066a60d
Nicholas  Luong <nckluong@gmail.com>
2018-06-02 21:52:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   var play = []
   for(let i = 0 ; i < musicians.length ; i++){
-    play.push(`"${musicians[i]} plays ${instruments[i]}"`)
+    play.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return play
 }
